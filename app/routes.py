5a36271bb05ecd0b64e377181041a9d1a5e2f1e7
Juan Oriana <joriana@itba.edu.ar>
2020-06-28 01:40:53 -0300

    Se agregaron iconos y css

--- app/routes.py
+++ app/routes.py
@@ -2,18 +2,20 @@ import json,requests,math,os
 from app import app
 from flask import render_template,request
 
-WEATHER_KEY = "os.environ.get('WEATHER_KEY')"
+WEATHER_KEY = os.environ.get('WEATHER_KEY')
 
 @app.route('/')
 @app.route('/index')
 def index():
-    loc_json = json.loads("http://www.geoplugin.net/json.gp?")
+    loc_request = requests.get("http://www.geoplugin.net/json.gp?ip=95.172.233.72")
+    loc_json = json.loads(loc_request.text)
     wea_request = requests.get("http://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}"
     .format(loc_json['geoplugin_latitude'],loc_json['geoplugin_longitude'],WEATHER_KEY))
     weat_json = json.loads(wea_request.text)
     return render_template("index.html",
     city=loc_json['geoplugin_city'],
-    weather=weat_json['weather'][0]['main'],
+    image= "static/img/icons/{}.png".format(weat_json['weather'][0]['icon']),
+    description=weat_json['weather'][0]['description'],
     temp=math.floor(weat_json['main']['temp']-273)
     )
 
